b1e714ebf86bf21a24cc6b3fcbe486bf15c85e00
SumanthReddy19 <sumanth.lreddy1909@gmail.com>
2024-11-12 09:19:32 +0530

    complted the Add Doctor UI

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -3,12 +3,30 @@ import Login from "./pages/login";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { AdminContext } from "./context/AdminContext";
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import { Route, Routes } from "react-router-dom";
+import DashBoard from "./pages/Admin/DashBoard";
+import AllAppointments from "./pages/Admin/AllAppointments";
+import AddDoctor from "./pages/Admin/AddDoctor";
+import DoctorsList from "./pages/Admin/DoctorsList";
 
 const App = () => {
   const { aToken } = useContext(AdminContext);
   return aToken ? (
-    <div>
+    <div className="bg-[#F8F9FD]">
       <ToastContainer />
+      <NavBar/>
+      <div className="flex items-start">
+        <SideBar/>
+        <Routes>
+          <Route path="/" element={<></>}/>
+          <Route path="/admin-dashboard" element={<DashBoard/>}/>
+          <Route path="/all-appointments" element={<AllAppointments/>}/>
+          <Route path="/add-doctor" element={<AddDoctor/>}/>
+          <Route path="/doctors-list" element={<DoctorsList/>}/>
+        </Routes>
+      </div>
     </div>
   ) : (
     <>
