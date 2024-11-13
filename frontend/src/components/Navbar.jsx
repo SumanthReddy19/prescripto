09df40ca69ca7b31efaa4c0f0bc37749275d0e9c
SumanthReddy19 <sumanth.lreddy1909@gmail.com>
2024-11-13 10:16:22 +0530

    Update functionality for user

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from "../context/AppContext";
 const Navbar = () => {
   const navigate = useNavigate();
   const [showMenu, setShowMenu] = useState(false);
-  const {token,setToken} = useContext(AppContext)
+  const {token,setToken,userData} = useContext(AppContext)
 
   const logout = () =>{
     setToken(false)
@@ -37,7 +37,7 @@ const Navbar = () => {
       <div className="flex items-center gap-4">
         {token ? (
           <div className="flex items-center gap-2 cursor-pointer group relative">
-            <img src={assets.profile_pic} alt="" className="w-8 rounded-full" />
+            <img src={userData.image} alt="" className="w-8 rounded-full" />
             <img src={assets.dropdown_icon} alt="" className="w-2.5" />
             <div className="absolute top-0 right-0 pt-14 text-base text-gray-600 z-20 font-medium hidden group-hover:block">
               <div className="min-w-48 bg-stone-100 rounded flex flex-col p-4 gap-4">
