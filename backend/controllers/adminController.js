cb8186a2cbe5e98c97e0fce2561511d8f913d6c6
SumanthReddy19 <sumanth.lreddy1909@gmail.com>
2024-11-18 12:12:33 +0530

    Display and Manage Appointment for Admin panel

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -3,6 +3,7 @@ import bcrypt from 'bcrypt'
 import { v2 as cloudinary } from "cloudinary";
 import doctorModel from "../models/doctorModel.js";
 import jwt from 'jsonwebtoken'
+import appointmentModel from "../models/appointmentModel.js";
 //API for adding doctor
 
 const addDoctor = async(req,res)=>{
@@ -85,4 +86,32 @@ try {
 }
 }
 
-export {addDoctor,loginAdmin,allDoctors}
+//API to get all appointments list
+const appointmentsAdmin = async (req,res) => {
+    try {
+        const appointments = await appointmentModel.find({})
+        res.json({
+            success:true,appointments
+        })
+    } catch (error) {
+        res.json({success:false,message:error})
+        console.log(error)
+    }
+}
+
+//API for appointment cancellation
+const appointmentCancel = async (req, res) => {
+    try {
+
+        const { appointmentId } = req.body
+        await appointmentModel.findByIdAndUpdate(appointmentId, { cancelled: true })
+
+        res.json({ success: true, message: 'Appointment Cancelled' })
+
+    } catch (error) {
+        console.log(error)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export {addDoctor,loginAdmin,allDoctors,appointmentsAdmin,appointmentCancel}
